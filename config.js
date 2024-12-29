ffab6404fe3b23d9e167c187d34e69a56f0b914f
NakhunHost <presidentialrealmsstudio@gmail.com>
2024-12-29 15:53:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,12 +39,12 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "lavalink.oddcoder.xyz",
+        port: 443,
+        password: "oddcoder",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
-        secure: false
+        secure: true
     },
  ]
 }
